08492dda7387effd2edbfd79ab801fe3f45e8f6f
bt853210 <bt853210@gmail.com>
2019-04-11 13:07:40 +0800

    計算.換圖片

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,13 +5,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private EditText ed1,ed2,ed3;
+    private  TextView txv;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getWindow().setBackgroundDrawable(R.drawable.dog);
     }
 
     int size=15;
@@ -24,7 +28,34 @@ public class MainActivity extends AppCompatActivity {
         ed3=findViewById(R.id.editText3);
         txv=findViewById(R.id.editText);
 
+        float height = Float.parseFloat(ed2.getText().toString());
+        float weight = Float.parseFloat(ed2.getText().toString());
+        float BMI1 = calculateBMI(height,weight);
+        String BMI2 = String.valueOf(BMI1);
+
         txv.setTextSize(size);
-        txv.setText(ed1.getText().toString()+",您好\n"+"您的BMI是:");
+        txv.setText(ed1.getText().toString()+",您好\n"+"您的BMI是:"+BMI2);
+        Toast.makeText(context:this, standardBMI(BMI1),Toast.LENGTH_LONG).show();
+    }
+
+    private float calculateBMI(float ht,float wt){
+        float BMI=(float)(wt/Math.pow((ht/100),2));
+        return BMI;
+    }
+
+    private String standardBMI(double st){
+        String alert="";
+
+        if(st>25){
+            alert = String.valueOf("You are too heavy!!!");
+            getWindow().setBackgroundDrawable(R.drawable.dog);
+        }
+        else if(st<18.5){
+            alert = String.valueOf("You are too slim.");
+            getWindow().setBackgroundDrawable(R.drawable.dog);
+        }
+        else {
+            alert = String.valueOf("You body is good");
+        }
     }
 }
